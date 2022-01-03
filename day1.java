c91c44bc99f028996f6b06695f9e5e77c97b3666
hwajin-jo <70902597+hwajin-jo@users.noreply.github.com>
2022-01-03 13:21:22 +0900

    codeup100제_1일차

---
+++ day1.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class day1 {
+
+// no 1.
+//    public static void main(String[] args) {
+//        System.out.print("Hello");
+//    }
+
+
+// no 2.
+//    public static void main(String[] args) {
+//        System.out.print("Hello World");
+//    }
+
+// no 3.
+//    public static void main(String[] args) {
+//        System.out.println("Hello");
+//        System.out.println("World");
+//    }
+
+// no 4.
+//    public static void main(String[] args) {
+//        System.out.print("'Hello'");
+//    }
+
+// no 5.
+//    public static void main(String[] args) {
+//        System.out.print("\"Hello World\"");
+//    }
+
+// no 6.
+//    public static void main(String[] args) {
+//        System.out.print("\"!@#$%^&*()\"");
+//    }
+
+// no 7.
+//    public static void main(String[] args) {
+//        System.out.print("\"C:\\Download\\hello.cpp\"");
+//    }
+
+// no 8.
+//    public static void main(String[] args) {
+//        System.out.println("\u250C\u252C\u2510");
+//        System.out.println("\u251C\u253C\u2524");
+//        System.out.println("\u2514\u2534\u2518");
+//    }
+
+// no 10.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.print(num);
+    }
+}
+
