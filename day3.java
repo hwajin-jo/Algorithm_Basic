def429f36102784ca72b477e58aa6168270bfe40
hwajin-jo <70902597+hwajin-jo@users.noreply.github.com>
2022-01-06 20:47:44 +0900

    day3 commit

---
+++ day3.java
@@ -0,0 +1,116 @@
+import jdk.jfr.Unsigned;
+
+import java.util.Scanner;
+
+public class day3 {
+
+    // no.21
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        System.out.println(str);
+//    }
+
+    // no.22
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        System.out.println(str);
+//    }
+
+    // no.23
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String num = sc.next();
+//        String[] a = num.split("\\.");
+//
+//        System.out.println(a[0]);
+//        System.out.println(a[1]);
+//
+//    }
+
+    // no.24
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.equals(' ')) {
+//                break;
+//            } else {
+//                System.out.printf("'%s'",str.charAt(i));
+//                System.out.println();
+//            }
+//        }
+//    }
+
+    // no.25
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//
+//        int a1 = (num / 10000) * 10000; // 75254 % 10000 = 70000
+//        int b1 = num % 10000; // 5254
+//        int a2 = (b1 / 1000) * 1000; // 5000
+//        int b2 = b1 % 1000; // 254
+//        int a3 = (b2 / 100) * 100; // 200
+//        int b3 = b2 % 100; // 54
+//        int a4 = (b3 / 10) * 10; // 50
+//        int b4 = b3 % 10; // 4
+//
+//        System.out.printf("[%d]", a1);
+//        System.out.println();
+//        System.out.printf("[%d]", a2);
+//        System.out.println();
+//        System.out.printf("[%d]", a3);
+//        System.out.println();
+//        System.out.printf("[%d]", a4);
+//        System.out.println();
+//        System.out.printf("[%d]", b4);
+//
+//    }
+
+    // no.26
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String s = sc.next();
+//        String [] arr = s.split(":");
+//
+//        System.out.println(Integer.parseInt(arr[1]));
+//
+//    }
+
+    // no.27
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//        String [] arr = str.split("\\.");
+//
+//        System.out.printf("%s-%s-%s", arr[2], arr[1], arr[0]);
+//    }
+
+    // no.28
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        long num = sc.nextLong();
+//
+//        System.out.println(num);
+//
+//    }
+
+    // no.29
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        Double num = sc.nextDouble();
+//
+//        System.out.printf("%.11f", num);
+//    }
+
+    // no.30
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        long num = sc.nextLong();
+//
+//        System.out.println(num);
+//    }
+}
