c4c3f0a19e9f9df42a7598d4003083d58707e74e
hwajin-jo <70902597+hwajin-jo@users.noreply.github.com>
2022-01-07 12:26:45 +0900

    day4 commit

---
+++ day4.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class day4 {
+
+    // no.31
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        System.out.printf("%o", num);
+//    }
+
+    // no.32
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        System.out.printf("%x", num);
+//    }
+
+    // no.33
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        System.out.printf("%X", num);
+//    }
+
+    // no.34
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//
+//        int num = Integer.valueOf(str, 8);
+//        System.out.printf("%d", num);
+//    }
+
+    // no.35
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//
+//        int num = Integer.valueOf(str, 16);
+//        System.out.printf("%o", num);
+//    }
+
+    // no.36
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//        char c = str.charAt(0);
+//        int num = (int)c;
+//        System.out.printf("%d", num);
+//    }
+
+    // no.37
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        char i = (char)num;
+//        System.out.print(i);
+//    }
+
+    // no.38
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        long num1 = sc.nextLong();
+//        long num2 = sc.nextLong();
+//        long result = num1 + num2;
+//        System.out.print(result);
+//    }
+
+    // no.39
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        long num1 = sc.nextLong();
+//        long num2 = sc.nextLong();
+//        System.out.print(num1+num2);
+//    }
+
+    // no.40
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.printf("%d", -num);
+    }
+}
