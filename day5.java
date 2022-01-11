c59c71cbaa26a0f3dac4a98d6d50645132eb9629
hwajin-jo <70902597+hwajin-jo@users.noreply.github.com>
2022-01-11 14:31:39 +0900

    day5 commit

---
+++ day5.java
@@ -0,0 +1,132 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
+public class day5 {
+
+    // no.41
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        char c = sc.next().charAt(0);
+//        int a = Integer.valueOf(c) + 1;
+//        char i = (char)a;
+//        System.out.println(i);
+//    }
+
+    //no.42
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        String[] data = str.split(" ");
+//
+//        int a = Integer.valueOf(data[0]);
+//        int b = Integer.valueOf(data[1]);
+//        System.out.println(a/b);
+//    }
+
+    //no.43
+//        public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        String[] data = str.split(" ");
+//
+//        int a = Integer.valueOf(data[0]);
+//        int b = Integer.valueOf(data[1]);
+//        System.out.println(a/b);
+//    }
+
+    //no.44
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        Long num = sc.nextLong();
+//        System.out.println(num+1);
+//    }
+
+    //no.45
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//
+//        String [] data = str.split(" ");
+//
+//        int a = Integer.valueOf(data[0]);
+//        int b = Integer.valueOf(data[1]);
+//
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+//        System.out.println(a/b);
+//        System.out.println(a%b);
+//        System.out.printf("%.2f", (float) a/b);
+//    }
+
+    //no.46
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        String[] data = str.split(" ");
+//        int a = Integer.valueOf(data[0]);
+//        int b = Integer.valueOf(data[1]);
+//        int c = Integer.valueOf(data[2]);
+//
+//        long sum = a + b + c;
+//        float avg = (float) sum / 3;
+//        System.out.println(sum);
+//        System.out.println(Math.round(avg*10)/10.0);
+//    }
+
+    //no.47
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        System.out.printf("%d", num<<1);
+//    }
+
+    //no.48
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        String[] data = str.split(" ");
+//
+//        int a = Integer.valueOf(data[0]);
+//        int b = Integer.valueOf(data[1]);
+//        int b2 = 1;
+//
+//        for(int i = 0; i < b; i++) {
+//            b2 *= 2;
+//        }
+//
+//        int result = a * b2;
+//        System.out.println(result);
+//    }
+
+    //no.49
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        String[] data = str.split(" ");
+//
+//        int a = Integer.valueOf(data[0]);
+//        int b = Integer.valueOf(data[1]);
+//
+//        if (a > b) {
+//            System.out.println(1);
+//        } else {
+//            System.out.println(0);
+//        }
+//    }
+
+    //no.50
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        String[] data = str.split(" ");
+//
+//        int a = Integer.valueOf(data[0]);
+//        int b = Integer.valueOf(data[1]);
+//        if (a == b) {
+//            System.out.println(1);
+//        } else {
+//            System.out.println(0);
+//        }
+//    }
+}
